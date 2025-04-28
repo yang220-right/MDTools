b131a6ab7ae1d6dde36759a045a035a7ae7fd278
猫 <yumeng@51cashbox.com>
2025-04-28 17:24:30 +0800

    fix 测试提交

---
+++ TestTools/Assets/Runtime/TestScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript
+{
+    public static void Run()
+    {
+        Debug.Log("GD");
+    }
+}
